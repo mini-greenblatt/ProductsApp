937bbc350a02b79f2b2e93df310790f5d6b51351
mini-greenblatt <minig1045@gmail.com>
2025-11-02 23:28:11 +0200

    4

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+const config = {
+
+};
+
+module.exports = mergeConfig(defaultConfig, config);
